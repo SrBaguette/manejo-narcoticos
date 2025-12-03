f9c5f0483b5fc5acfae3c9b895b2622793b04819
Valentina Sandoval <Valen>
2025-12-03 11:01:20 -0500

    Feat: Se añadio un metodo nuevo mayor_municipio y menor_departamento

--- admindatos.py
+++ admindatos.py
@@ -4,3 +4,35 @@ class AdminDatos:
     @staticmethod
     def leer_csv(archivocsv):
         return pd.read_csv(archivocsv)
+    
+    @staticmethod
+    def mayor_municipio(df):
+        conteo = df['municipio'].value_counts()
+        municipio_top = conteo.idxmax()
+        dosis_municipio = df[df['municipio'] == municipio_top]['dosis_g'].sum()
+
+        print("Municipio con mayor incautacion: ")
+        print(f"Municipio: {municipio_top}")
+        print(f"Dosis: {dosis_municipio}")
+
+        return municipio_top, dosis_municipio
+    
+    @staticmethod
+    def menor_departamento(df):
+        conteo = df['departamento'].value_counts()
+        departamento_top = conteo.idxmax()
+        dosis_departamento = df[df['departamento'] == departamento_top]['dosis_g'].min()
+
+        print("Departamento con menor incautacion: ")
+        print(f"Departamento: {departamento_top}")
+        print(f"Dosis: {dosis_departamento}")
+
+        return departamento_top, dosis_departamento
+
+
+
+
+
+
+    
+
