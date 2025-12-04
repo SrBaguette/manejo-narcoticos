7096903926cc96c8159b69ed404247b67ee13437
Jose serna <hernandoserna453>
2025-12-04 08:21:55 -0500

    feat: Se añadieron nuevas funciones al admindatos para obtener resultados del departamento

--- admindatos.py
+++ admindatos.py
@@ -6,14 +6,25 @@ class AdminDatos:
         return pd.read_csv(archivocsv)
 
     @staticmethod
-    def mayor_incautacion(df):
-        return df.loc[df["departamento","dosis_g"].idxmax()]
-        
+    def mayor_departamento(df):
+        conteo = df.groupby('departamento').size()
+        depto_max = conteo.idxmax()
+        dosis_max = df[df['departamento'] == depto_max]['dosis_g'].max()
+        print("Departamento con mayor incautación:")
+        print(f"Departamento: {depto_max}")
+        print(f"Dosis: {dosis_max}")
+        return depto_max, dosis_max
 
     @staticmethod
-    def menor_incautacion(df):
-        return df.loc[df["dosis_g"].idxmin()]
-        
+    def menor_departamento(df):
+        conteo = df.groupby('departamento').size()
+        depto_min = conteo.idxmin()
+        dosis_min = df[df['departamento'] == depto_min]['dosis_g'].min()
+        print("Departamento con menor incautación:")
+        print(f"Departamento: {depto_min}")
+        print(f"Dosis: {dosis_min}")
+        return depto_min, dosis_min
+            
 
     
     
