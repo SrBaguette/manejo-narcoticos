ed70116ccd603a0c68aeff654403f9d0f6b5cff4
Jose serna <hernandoserna453>
2025-12-04 10:23:44 -0500

    feat:Se añadieron graficas y se arreglo una funcion en admindatos

--- graficas.py
+++ graficas.py
@@ -1,3 +1,23 @@
 from admindatos import AdminDatos
+import matplotlib.pyplot as plt
 class Graficas:
     pass
+
+    @staticmethod
+    def Grafica_Departamento_Mayor(df):
+        datos = df.groupby('departamento')['dosis_g'].max().sort_values().tail(10)
+        datos.plot(kind='bar', title='Dosis incautada por departamento')
+        plt.xlabel('Departamento')
+        plt.ylabel('Dosis_g')
+        plt.tight_layout()
+        plt.show()
+
+    @staticmethod
+    def Grafica_Departamento_Menor(df):
+        datos = df.groupby('departamento')['dosis_g'].min().sort_values().tail(10)
+        datos.plot(kind='bar', title='Dosis incautada por departamento')
+        plt.xlabel('Departamento')
+        plt.ylabel('Dosis_g')
+        plt.tight_layout()
+        plt.show()
+
