edfabc7d4471a026a7db6864919cff0e1dedb3c4
Jean Pierre <SrBaguette>
2025-12-04 10:33:40 -0500

    feat: creación de submenú de graficas y nuevo metodo para mostrar incautaciones por mes

--- graficas.py
+++ graficas.py
@@ -1,3 +1,15 @@
 from admindatos import AdminDatos
+import pandas as pd
+import matplotlib.pyplot as plt
 class Graficas:
-    pass
+    @staticmethod
+    def pormesgra(df):
+        df['fecha'] = pd.to_datetime(df['fecha'], format='%d/%m/%Y')
+        mensual = df.groupby(df['fecha'].dt.to_period('D'))["dosis_g"].sum()
+        plt.figure(figsize=(10,5))
+        mensual.plot(kind='bar')
+        plt.xlabel('Dia')
+        plt.ylabel('Gramos incautados')
+        plt.title('Incautaciones por dias')
+        plt.tight_layout()
+        plt.show()
