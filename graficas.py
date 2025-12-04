a7a6b3a228a910624540246e373550ceed3df311
Ivan Aldana <ialdanacon>
2025-12-04 10:40:01 -0500

    Feat: Se crea la grafica para los quartiles, la grafica utilizada es caja y bigotes Escala Logaritmica

--- graficas.py
+++ graficas.py
@@ -1,3 +1,14 @@
 from admindatos import AdminDatos
+import matplotlib.pyplot as plt
 class Graficas:
     pass
+ 
+    @staticmethod
+    def Grafica_quartiles(df):
+        plt.figure(figsize=(10, 7))
+        plt.boxplot(df["dosis_g"])
+        plt.yscale("log") 
+        plt.title("Caja y Bigotes (Escala Logarítmica)")
+        plt.ylabel("dosis_g (log)")
+        plt.show()
+
